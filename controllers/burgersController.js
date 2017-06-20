daa7745f9d5a71e3d296c1c8dfbc8c12f5c95f1f
Rochelle Beal <bealrochelle@gmail.com>
2017-06-20 08:28:41 -0500

    Ready for deployment

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -7,33 +7,55 @@ var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  db.Burger.findAll({}).then(function(data){
-    console.log(data);
+  db.Burger.findAll({
+    include: [{model: db.Eater}]
+  }).then(function(data){
+    //console.log(data);
     res.render("index", {burger: data});
   })
 });
 
+//Create burgers
 router.post("/", function(req, res) {
   db.Burger.create({
       burger_name: req.body.burger_name
-  }).then(function(result){
+  }).then(function(burgerData){
+    console.log(burgerData);
     res.redirect('/');
  });
 });
 
 router.put("/:id", function(req, res) {
-  db.Burger.update({
-    //burger_name: req.body.burger_name,
-    devoured: req.body.devoured
-  }, {
+  db.Eater.findOne({
     where: {
-      id: req.params.id
+      eater_name: req.body.eater
     }
-  }).then(function(data){
-     res.redirect('/');
-  });
-
+  }).then(function(eater){
+    if (eater){
+      updateBurger(eater, id, req, res);
+    }else {
+      db.Eater.create({
+        eater_name: req.body.eater_name
+      }).then(function(eater){
+          updateBurger(eater, id, req, res);
+      })
+    }
+  })
 });
+
+function updateBurger(eater, id, req, res) {
+	db.Burger.update(
+		{
+			devoured: req.body.devoured,
+			EaterId: eater.id
+		}, {
+			where: {
+			id: id
+		}
+	}).then(function(result) {
+		res.redirect('/');
+	});
+}
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -3,19 +3,33 @@ module.exports = function(sequelize, DataTypes) {
     burger_name: {
       type: DataTypes.STRING,
       allowNull: false
+      /*validate:{
+                isAlpha: true,    // will only allow letters
+                notEmpty: true,   // don't allow empty strings
+                len: [1,20]      // only allow values with length between 1 and 20
+            }*/
     },
     devoured: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
-      
     },
     date: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW
     }
 
-  },{
-    timestamps: false
+  },
+    {
+      timestamps: false,
+      classMethods: {
+        associate: function(models){
+          Burger.belongsTo(models.Eater, {  //will add a EaterId attribute to Burger to hold the primary key value of for Eater
+            foreignKey: {
+              allowNull: false
+            }
+          });
+        }
+      }
   });
   return Burger;
 };
